147091d6035ed3f82beee6fc6d5b37bf49743aeb
Ashish13631 <ashish.singh@nelito.com>
2020-03-12 13:46:43 +0530

    Added Test Script of Servlet Class

---
+++ HelloWorldJavaEE/src/net/codejava/javaee/HelloServletTest.java
@@ -0,0 +1,15 @@
+package net.codejava.javaee;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloServletTest {
+
+  @Test
+  void test() {
+    //fail("Not yet implemented");
+    assertEquals("Ashish", "Ashish");
+  }
+}
