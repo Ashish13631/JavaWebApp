6841356d370bc87d1404e64501b702d357bbf2f4
Ashish13631 <ashish.singh@nelito.com>
2020-03-12 17:26:59 +0530

    added junit to Pom.xml

--- HelloWorldJavaEE/src/net/codejava/javaee/HelloServletTest.java
+++ HelloWorldJavaEE/src/net/codejava/javaee/HelloServletTest.java
@@ -1,9 +1,8 @@
 package net.codejava.javaee;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class HelloServletTest {
 
